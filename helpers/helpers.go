5c93ee0da0f54e71c6d53f00630882d9284734e8
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-11-17 15:33:55 -0300

    Update helpers.go

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -14,24 +14,29 @@ func DecodeBody(req *http.Request) (models.Status, bool) {
 	if err != nil {
 		return models.Status{}, false
 	}
-
 	return status, true
 }
 
 func IsValidEntry(entry string) bool {
 	desc := strings.TrimSpace(entry)
-	if len(desc) == 0 {
-		return false
-	}
-	return true
+	return len(desc) != 0
 }
 
 func IsValidEntryTimestamp(entry string) bool {
 	desc := strings.TrimSpace(entry)
 	_, err := time.Parse("2006-01-02 15:04:05.999", desc)
+	return err == nil
+}
+
+func IsTimeInInterval(entry string, interval int) bool {
+	loc, _ := time.LoadLocation("America/Santiago")
+	desc := strings.TrimSpace(entry)
+	tMsg, err := time.ParseInLocation("2006-01-02 15:04:05.999", desc, loc)
+	tNow := time.Now()
 	if err != nil {
 		return false
 	}
+	diff := tNow.Sub(tMsg)
 
-	return true
+	return diff > time.Minute*time.Duration(interval)
 }
