6571f6672a60ec872cb6cd4f5d17ca0306dec49f
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2022-04-26 11:17:35 -0400

    Update Alerts.go

--- models/Alerts.go
+++ models/Alerts.go
@@ -7,36 +7,38 @@ import (
 )
 
 type Status struct {
-	Cc_name      string `json:"cc_name"`
-	Ds_status    string `json:"ds_status"`
-	Faust_status string `json:"faust_status"`
-	Timestamp    string `json:"timestamp"`
+	Cc_name            string `json:"cc_name"`
+	Ds_status_flujo    string `json:"ds_status_flujo"`
+	Ds_status_aforo    string `json:"ds_status_aforo"`
+	Faust_status_flujo string `json:"faust_status_flujo"`
+	Faust_status_aforo string `json:"faust_status_aforo"`
+	Timestamp          string `json:"timestamp"`
 }
 
 func Get(id string) (Status, bool) {
 	db := database.GetConnection()
 	defer db.Close()
-
-	row := db.QueryRow("SELECT * FROM server_status WHERE cc_name = $1", id)
+	row := db.QueryRow("SELECT * FROM server_statusV2 WHERE cc_name = $1", id)
 
 	var cc_name sql.NullString
-	var ds_status sql.NullString
-	var faust_status sql.NullString
+	var ds_status_flujo sql.NullString
+	var ds_status_aforo sql.NullString
+	var faust_status_flujo sql.NullString
+	var faust_status_aforo sql.NullString
 	var timestamp sql.NullString
 
-	err := row.Scan(&cc_name, &ds_status, &faust_status, &timestamp)
+	err := row.Scan(&cc_name, &ds_status_flujo, &ds_status_aforo, &faust_status_flujo, &faust_status_aforo, &timestamp)
 	if err != nil {
 		return Status{}, false
 	}
 
-	return Status{cc_name.String, ds_status.String, faust_status.String, timestamp.String}, true
+	return Status{cc_name.String, ds_status_flujo.String, ds_status_aforo.String, faust_status_flujo.String, faust_status_aforo.String, timestamp.String}, true
 }
 
 func GetAll() []Status {
 	db := database.GetConnection()
 	defer db.Close()
-
-	rows, err := db.Query("SELECT * FROM server_status")
+	rows, err := db.Query("SELECT * FROM server_statusV2")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,18 +50,22 @@ func GetAll() []Status {
 		t := Status{}
 
 		var cc_name sql.NullString
-		var ds_status sql.NullString
-		var faust_status sql.NullString
+		var ds_status_flujo sql.NullString
+		var ds_status_aforo sql.NullString
+		var faust_status_flujo sql.NullString
+		var faust_status_aforo sql.NullString
 		var timestamp sql.NullString
 
-		err := rows.Scan(&cc_name, &ds_status, &faust_status, &timestamp)
+		err := rows.Scan(&cc_name, &ds_status_flujo, &ds_status_aforo, &faust_status_flujo, &faust_status_aforo, &timestamp)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		t.Cc_name = cc_name.String
-		t.Ds_status = ds_status.String
-		t.Faust_status = faust_status.String
+		t.Ds_status_flujo = ds_status_flujo.String
+		t.Ds_status_aforo = ds_status_aforo.String
+		t.Faust_status_flujo = faust_status_flujo.String
+		t.Faust_status_aforo = faust_status_aforo.String
 		t.Timestamp = timestamp.String
 
 		status = append(status, t)
@@ -73,41 +79,16 @@ func GetAll() []Status {
 	return status
 }
 
-func Update(id string, ds_status string, faust_status string, timestamp string) (Status, bool) {
+func Update(id string, ds_status_flujo string, ds_status_aforo string, faust_status_flujo string, faust_status_aforo string, timestamp string) (Status, bool) {
 	db := database.GetConnection()
 	defer db.Close()
 
 	var cc_id sql.NullString
-	db.QueryRow("UPDATE server_status SET ds_status = $1, faust_status = $2, timestamp = $3 WHERE cc_name = $4 RETURNING cc_name", ds_status, faust_status, timestamp, id).Scan(&cc_id)
+	db.QueryRow("UPDATE server_statusV2 SET ds_status_flujo = $1, ds_status_aforo = $2, faust_status_flujo = $3, faust_status_aforo = $4, timestamp = $5 WHERE cc_name = $6 RETURNING cc_name",
+		ds_status_flujo, ds_status_aforo, faust_status_flujo, faust_status_aforo, timestamp, id).Scan(&cc_id)
 	if cc_id.String == "" {
 		return Status{}, false
 	}
 
-	return Status{cc_id.String, ds_status, faust_status, timestamp}, true
+	return Status{cc_id.String, ds_status_flujo, ds_status_aforo, faust_status_flujo, faust_status_aforo, timestamp}, true
 }
-
-/* func Delete(id string) (Todo, bool) {
-	db := database.GetConnection()
-
-	var todo_id int
-	db.QueryRow("DELETE FROM todos WHERE id = $1 RETURNING id", id).Scan(&todo_id)
-	if todo_id == 0 {
-		return Todo{}, false
-	}
-
-	return Todo{todo_id, ""}, true
-}
-
-func Insert(description string) (Todo, bool) {
-	db := database.GetConnection()
-
-	var todo_id int
-	db.QueryRow("INSERT INTO todos(description) VALUES($1) RETURNING id", description).Scan(&todo_id)
-
-	if todo_id == 0 {
-		return Todo{}, false
-	}
-
-	return Todo{todo_id, ""}, true
-}
-*/
